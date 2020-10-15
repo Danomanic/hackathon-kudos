9e00cf5ec68806f78d89a19ef301eceb2f9b29bf
daniel.pomfret <daniel.pomfret@dwp.gov.uk>
2020-10-15 13:46:57 +0100

    Fix heroku

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({
     extended: true
 }));
