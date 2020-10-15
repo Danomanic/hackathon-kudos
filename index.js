a44f9925e4c2906b4b33d28090c7cf1ea76aa093
daniel.pomfret <daniel.pomfret@dwp.gov.uk>
2020-10-15 13:32:04 +0100

    add mongodb

--- index.js
+++ index.js
@@ -1,26 +1,41 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mustacheExpress = require('mustache-express');
+const db = require('monk')(process.env.DB_HOST);
+
 app.use(express.static(__dirname + '/public'));
 app.engine('html', mustacheExpress());
 app.set('view engine', 'html');
 app.set('views', __dirname + '/views');
 const port = 3000;
-app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get('/', (req, res) => {
-       res.render('index.html')
-   });
+const kudos = db.get('kudos');
 
-app.post('/thankyou', (req, res) => {
+app.get('/', async (req, res) => {
+    res.render('index.html')
+});
+
+app.post('/thankyou', async (req, res) => {
+    const submission = req.body;
+    kudos.insert({
+        nominee: submission.nominee,
+        nominator: submission.nominator,
+        reason: submission.reason
+    })
     res.render('thankyou.html')
 })
 
+// Helper endpoint
+app.get('/list', async (req, res) => {
+    res.send(await kudos.find({}));
+});
 
 
 app.listen(port, () => {
     console.log("'the server is listening on port 3000");
 })
-
-
